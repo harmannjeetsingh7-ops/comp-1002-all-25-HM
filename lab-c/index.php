79ede1ecb889449a5da89c0a9ecdd60dc56d5293
Harmanjeet singh <harmannjeet.singh7@gmail.com>
2025-10-31 20:22:13 -0400

    lab c

--- lab-c/index.php
+++ lab-c/index.php
@@ -12,7 +12,7 @@
 	<body id="section1">
 		<!-- Page-level header -->
 		<header>
- 			<img src="images/html-logo.svg" height="75" width="75" alt="HTML" />
+ 			<img src="images/html-logo.svg" height="100" width="100" alt="HTML" />
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 <?php
